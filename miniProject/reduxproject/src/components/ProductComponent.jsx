7ada9a62734db03e07ee5102a4ad7a96ae4b4fea
Sameer <sameermanapkar24@gmail.comm>
2023-04-26 09:49:33 +0530

    final draft

--- miniProject/reduxproject/src/components/ProductComponent.jsx
+++ miniProject/reduxproject/src/components/ProductComponent.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const ProductComponent = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
+
   const products = useSelector((store) => store.allProducts.product);
-  const renderList = products.map((product) => {
+
+  // Filter products by category
+  const filteredProducts = selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products;
+
+  // Sort products by price
+  const sortedProducts = sortOrder
+    ? [...filteredProducts].sort((a, b) =>
+        sortOrder === "asc" ? a.price - b.price : b.price - a.price
+      )
+    : filteredProducts;
+
+  const renderList = sortedProducts.map((product) => {
     const { id, title, image, price, category } = product;
     return (
       <div className="four wide column" key={id}>
@@ -27,7 +43,47 @@ const ProductComponent = () => {
     );
   });
 
-  return <>{renderList}</>;
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleSortOrderChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="category">Filter by Category:</label>
+        <select
+          id="category"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="">All</option>
+          <option value="electronics">Electronics</option>
+          <option value="jewelery">Jewelery</option>
+          <option value="men's clothing">Men's Clothing</option>
+          <option value="women's clothing">Women's Clothing</option>
+        </select>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="sort-order">Sort by Price:</label>
+        <select
+          id="sort-order"
+          value={sortOrder}
+          onChange={handleSortOrderChange}
+        >
+          <option value="">None</option>
+          <option value="asc">Low to High</option>
+          <option value="desc">High to Low</option>
+        </select>
+      </div>
+      <br />
+      <div className="ui grid container">{renderList}</div>
+    </div>
+  );
 };
 
 export default ProductComponent;
